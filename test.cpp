6a6f828da90ff6eb44be1bed6d14ed738b1aba76
Keelin Becker-Wheeler <keelinwheeler@gmail.com>
2017-06-22 23:20:40 -0400

    Figuring out mouse input

--- test.cpp
+++ test.cpp
@@ -2,13 +2,18 @@
 #include <fstream>
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
 #include <linux/fb.h>
 #include <linux/kd.h>
+#include <linux/input.h>
 #include <sys/ioctl.h>
 
 #include "test_container.h"
+
+#define MOUSE_MOVE_FILE "/dev/input/event7"
+#define MOUSE_CLICK_FILE "/dev/input/mouse0"
 
 void render(litehtml::uint_ptr hdc, litehtml::document::ptr doc, test_container painter, struct fb_var_screeninfo vinfo);
 bool update(litehtml::document::ptr doc);
@@ -19,46 +24,77 @@ int main(int argc, char* argv[]) {
 		std::cout << "usage: " << argv[0] << " <html_file> <master_css>" << std::endl;
 	} else {
 
-		/* Read the given file and print its contents to the screen */
-		std::cout << "Reading file " << argv[1] << " as HTML:" << std::endl << std::endl;
+	    struct input_event move_ie;
+	    unsigned char click_ie;
 
-		std::string path = argv[1];
-		std::string prefix = "";
-		if (std::count(path.begin(), path.end(), '/') > 0)
-			prefix = path.substr(0, path.find_last_of('/'));
+		int mmf, mcf;
+		if ((mmf = open(MOUSE_MOVE_FILE, O_RDONLY))==-1) {
+			std::cout << "ERROR opening MOUSE_MOVE_FILE" << std::endl;
+			mmf = 0;
+		} else if ((mcf = open(MOUSE_CLICK_FILE, O_RDONLY))==-1) {
+			std::cout << "ERROR opening MOUSE_CLICK_FILE" << std::endl;
+			mmf = 0;
+			mcf = 0;
+		}
 
-		std::ifstream t(argv[1]);
-		std::stringstream buffer;
-		buffer << t.rdbuf();
-		std::cout << buffer.str() << std::endl;
+		// /* Read the given file and print its contents to the screen */
+		// std::cout << "Reading file " << argv[1] << " as HTML:" << std::endl << std::endl;
 
-		/* Load the master.css file */
-		std::cout << "Loading file " << argv[2] << " as CSS" << std::endl << std::endl;
+		// std::string path = argv[1];
+		// std::string prefix = "";
+		// if (std::count(path.begin(), path.end(), '/') > 0)
+		// 	prefix = path.substr(0, path.find_last_of('/'));
 
-		std::ifstream t2(argv[2]);
-		std::stringstream buffer2;
-		buffer2 << t2.rdbuf();
+		// std::ifstream t(argv[1]);
+		// std::stringstream buffer;
+		// buffer << t.rdbuf();
+		// std::cout << buffer.str() << std::endl;
 
-		/* Get references to the framebuffer to work with */
-		struct fb_fix_screeninfo finfo;
-		struct fb_var_screeninfo vinfo;
-		litehtml::uint_ptr hdc = get_drawable(&finfo, &vinfo);
+		// /* Load the master.css file */
+		// std::cout << "Loading file " << argv[2] << " as CSS" << std::endl << std::endl;
 
-		test_container painter(prefix, &finfo, &vinfo);
-		litehtml::context context;
+		// std::ifstream t2(argv[2]);
+		// std::stringstream buffer2;
+		// buffer2 << t2.rdbuf();
 
-		/* Start litehtml rendering
-		   See: https://github.com/litehtml/litehtml/wiki/How-to-use-litehtml */
-		std::cout << "load_master_stylesheet" << std::endl;
-		context.load_master_stylesheet(buffer2.str().c_str());
-		std::cout << "createFromString" << std::endl;
-		litehtml::document::ptr doc;
-		doc = litehtml::document::createFromString(buffer.str().c_str(), &painter, &context);
+		// /* Get references to the framebuffer to work with */
+		// struct fb_fix_screeninfo finfo;
+		// struct fb_var_screeninfo vinfo;
+		// litehtml::uint_ptr hdc = get_drawable(&finfo, &vinfo);
+
+		// test_container painter(prefix, &finfo, &vinfo);
+		// litehtml::context context;
+
+		// /* Start litehtml rendering
+		//    See: https://github.com/litehtml/litehtml/wiki/How-to-use-litehtml */
+		// std::cout << "load_master_stylesheet" << std::endl;
+		// context.load_master_stylesheet(buffer2.str().c_str());
+		// std::cout << "createFromString" << std::endl;
+		// litehtml::document::ptr doc;
+		// doc = litehtml::document::createFromString(buffer.str().c_str(), &painter, &context);
 
 		bool done = false;
+
+		/*
+		mcf: 1 byte:
+            left = data[0] & 0x1;
+            right = data[0] & 0x2;
+            middle = data[0] & 0x4;
+
+		 mmf: type=3, time->time
+			code=0 -> x
+			code=1 -> y
+			value -> val
+		*/
 		while (!done) {
-			render(hdc, doc, painter, vinfo);
-			done = update(doc);
+			read(mcf, &click_ie, sizeof(unsigned char));
+			printf("click %d\n", click_ie);
+			if (click_ie&0x7) {
+				read(mmf, &move_ie, sizeof(struct input_event));
+				printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n", move_ie.time.tv_sec, move_ie.time.tv_usec, move_ie.type, move_ie.code, move_ie.value);
+			}
+			// render(hdc, doc, painter, vinfo);
+			// done = update(doc);
 		}
 		std::cout << "Completed." << std::endl;
 	}
